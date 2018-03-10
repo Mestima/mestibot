ebad12a3a93fd75a10a348152a1f154378813e39
Alex Fadeev <mestima@icloud.com>
2018-03-10 19:36:54 +0300

    Add files via upload
    simply changes in log class

--- mestima-bot/modules/log.lua
+++ mestima-bot/modules/log.lua
@@ -8,6 +8,7 @@ local emb  = require("./embed.lua")
 local log = {}
 
 function log:Send(msg)
+	if string.sub(msg.content,1,1) ~= prefix then return end -- we need to log bot commands only!
 	local channel = msg.client:getGuild("401864693704032256"):getChannel("412291174250840075")
 	local user = msg.author
 	local id = user.id
