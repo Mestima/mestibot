57177d71463e668cd9e35058107ba0beed62ddce
Alex Fadeev <mestima@icloud.com>
2018-07-22 00:23:27 +0300

    Created a prefix module

--- mestima-bot/bot.lua
+++ mestima-bot/bot.lua
@@ -9,7 +9,7 @@ local cl = ds.Client({
 ds.extensions()
 ---------------------------------------------[[ CONFIG ZONE ]]-----------------------------------------------
 prefix = "!"
-token = "TOKEN HERE" -- your bot token here
+token = "TOKEN HERE" -- your bot token here.
 -------------------------------------------------------------------------------------------------------------
 function reload()
 	-- i will realize it
